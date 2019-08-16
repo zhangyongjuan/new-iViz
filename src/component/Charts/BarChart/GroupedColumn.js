7d29f4d423d93ba685b0bbab03bae02600ebc9a7
dongkang <dongkang@jqs.com>
2019-08-16 11:36:52 +0800

    分组柱状图调整

--- src/component/Charts/BarChart/GroupedColumn.js
+++ src/component/Charts/BarChart/GroupedColumn.js
@@ -15,62 +15,80 @@ import {
   Util
 } from "bizcharts";
 import DataSet from "@antv/data-set";
+import _ from 'lodash';
 
 class GroupedColumn extends React.Component {
   render() {
     const data = [
       {
-        name: "London",
-        "Jan.": 18.9,
-        "Feb.": 28.8,
-        "Mar.": 39.3,
-        "Apr.": 81.4,
-        May: 47,
-        "Jun.": 20.3,
-        "Jul.": 24,
-        "Aug.": 35.6
+        label: "Monday",
+        series1: 2800,
+        series2: 2260
+      },
+
+      {
+        label: "Tuesday",
+        series3: 1800,
+        series4: 1300
       },
       {
-        name: "Berlin",
-        "Jan.": 12.4,
-        "Feb.": 23.2,
-        "Mar.": 34.5,
-        "Apr.": 99.7,
-        May: 52.6,
-        "Jun.": 35.5,
-        "Jul.": 37.4,
-        "Aug.": 42.4
+        label: "Wednesday",
+        series5: 950,
+        series6: 900
+      },
+      {
+        label: "Thursday",
+        series7: 500,
+        series8: 390
+      },
+      {
+        label: "Friday",
+        series9: 170,
+        series10: 100
       }
     ];
     const ds = new DataSet();
     const dv = ds.createView().source(data);
     dv.transform({
       type: "fold",
-      fields: ["Jan.", "Feb.", "Mar.", "Apr.", "May", "Jun.", "Jul.", "Aug."],
+      fields: ["series1", "series2","series3", "series4","series5", "series6","series7", "series8","series9", "series10"],
       // 展开字段集
-      key: "月份",
+      key: "type",
       // key字段
-      value: "月均降雨量" // value字段
+      value: "value" // value字段
     });
+
+    // const old = [...dv.rows];
+    // dv.rows = _.filter(old,(k)=>k.value!==undefined);
+    // console.log(dv.rows);
+    // dv.rows=[
+    //   {label:'Monday',type:'series1',value:2800},
+    //   {label:'Monday',type:'series3',value:2800},
+    //   {label:'Monday',type:'series4',value:2800},
+    //   {label:'Monday',type:'series6',value:2800},
+    //   {label:'Friday',type:'series5',value:2800},
+    //   {label:'Friday',type:'series7',value:2800},
+    //   ];
     return (
       <div>
-        <Chart height={400} data={dv} forceFit>
-          <Axis name="月份" />
-          <Axis name="月均降雨量" />
-          <Legend />
-          <Tooltip
-            crosshairs={{
-              type: "y"
+        <Chart height={400} data={dv} forceFit onPlotClick={(c)=>console.log(c)}>
+
+          <Axis
+            name="label"
+            label={{
+              offset: 12
             }}
           />
+          <Axis name="value" />
+          <Tooltip />
           <Geom
             type="interval"
-            position="月份*月均降雨量"
-            color={"name"}
+            position="label*value"
+            color={"type"}
             adjust={[
               {
                 type: "dodge",
-                marginRatio: 1 / 32
+                marginRatio: 0
               }
             ]}
           />
